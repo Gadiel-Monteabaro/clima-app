7818a58e246a43c8c7bc7260a97bb4188744184b
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-12-01 14:37:53 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="cards">
+          <div className="card first-card">
+            <section></section>
+            <div className="card-info"></div>
+          </div>
+          <div className="card second-card">
+            <section></section>
+            <div className="card-info"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
