f73e341f00b21e280c600b6780eefd7659e6a694
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-12-06 08:46:24 -0300

    style: establecer mejoras en el aspecto, agregar las secciones de copy, y github y subdivir la tarjeta en dos secciones

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ function App() {
       <div className="container">
         <div className="cards">
           <div className="card first-card">
-            <h2 className="card-title">clima</h2>
-            <Form />
+            <div>
+              <h2 className="card-title">clima</h2>
+              <Form />
+            </div>
+            <div className="card-info">
+              <p className="first-p-footer">
+                &copy; {new Date().getFullYear()} Gadiel Monteabaro
+              </p>
+              <a
+                href="https://github.com/Gadiel-Monteabaro/clima-app"
+                className="link-git"
+                target="_blank"
+              >
+                <i className="ri-github-fill">GitHub</i>
+              </a>
+            </div>
           </div>
           <div className="card second-card">
             <section></section>
