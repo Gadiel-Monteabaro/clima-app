59bb6d0f85465de3a92842d4507a4e4aa3b5890b
Gadiel-Monteabaro <silvagadiel96@gmail.com>
2024-12-06 11:59:50 -0300

    feat: Iniciarlizar estado "search" funcion handleChange, tomar los datos del formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,35 @@
+import { ChangeEvent, useState } from "react";
 import { countries } from "../data/countries";
+import type { SearchType } from "../types";
 
 export default function Form() {
+  // Inicializar el estado "search" con un objeto, que contiene las propiedades "city" y "country" sin valores
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+
+    console.log(search);
+  };
+
   return (
     <form className="form-clima">
       <div>
-        <select className="form-select inputs" id="country" name="country">
+        <select
+          className="form-select inputs"
+          id="country"
+          name="country"
+          value={search.city}
+          onChange={handleChange}
+        >
           <option value="">Seleccione un país</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
@@ -21,6 +46,8 @@ export default function Form() {
           type="text"
           placeholder="Ingrese una ciudad"
           autoComplete="off"
+          value={search.city}
+          onChange={handleChange}
         />
       </div>
       <input className="input-submit" type="submit" value={"Consultar clima"} />
